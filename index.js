82c4e2ddd572cf89f49e7a375e6ec800d6755938
DzakiDV <dzaki.viaski@gmail.com>
2024-12-03 22:43:16 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// Toggle hamburger menu
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
